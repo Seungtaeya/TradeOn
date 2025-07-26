3694451e4dfdc50ed396e22f6e3c8d7867f37557
seungtae <zkdlandgu@naver.com>
2025-07-26 21:29:31 +0900

    feat: 제품이미지 엔티티 생성

---
+++ src/main/java/com/ecommerce/tradeon/Entity/Image/ProductImage.java
@@ -0,0 +1,30 @@
+package com.ecommerce.tradeon.Entity.Image;
+
+import com.ecommerce.tradeon.Entity.product.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductImage {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private String imageUrl;
+
+    public ProductImage(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void assignProduct(Product product) {
+        this.product = product;
+    }
+}
