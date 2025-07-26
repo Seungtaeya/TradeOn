b4738fdde535dcfd88a1eed841416939d6bc57e3
seungtae <zkdlandgu@naver.com>
2025-07-26 21:29:41 +0900

    fix: 생성자 + 연관관계 메서드 추가

---
+++ src/main/java/com/ecommerce/tradeon/Entity/product/ProductOption.java
@@ -0,0 +1,31 @@
+package com.ecommerce.tradeon.Entity.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductOption {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private String name;
+    private String value;
+
+    public ProductOption(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public void assignProduct(Product) {
+        this.product = product;
+    }
+}
