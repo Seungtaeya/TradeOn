07145ea888869bc4d112880475e3b3bae1e76cff
seungtae <zkdlandgu@naver.com>
2025-07-30 23:50:07 +0900

    feat: 회원카운트 기능 구현

--- src/main/java/com/ecommerce/tradeon/Controller/Admin/AdminController.java
+++ src/main/java/com/ecommerce/tradeon/Controller/Admin/AdminController.java
@@ -1,15 +1,22 @@
 package com.ecommerce.tradeon.Controller.Admin;
 
+import com.ecommerce.tradeon.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
 
+    private final MemberService memberService;
+
     @GetMapping("/admin")
-    public String AdminHome() {
+    public String AdminHome(Model model) {
+        Long count = memberService.Count();
+        model.addAttribute("userCount", count);
+
         return "Admin/Dashboard";
     }
 }
