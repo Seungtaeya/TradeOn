0ba106d739d7029f420b1cc68339f1fa694f8f5c
seungtae <zkdlandgu@naver.com>
2025-07-30 23:52:18 +0900

    feat: 관리자 기능 추가

--- src/main/java/com/ecommerce/tradeon/Service/MemberService.java
+++ src/main/java/com/ecommerce/tradeon/Service/MemberService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.tradeon.Service;
 
+import com.ecommerce.tradeon.Dto.Member.AdminMemberDto;
 import com.ecommerce.tradeon.Dto.Member.myPageMemberDto;
 import com.ecommerce.tradeon.Entity.Member.Member;
 import com.ecommerce.tradeon.Exceptions.CustomeMemberException;
@@ -34,6 +35,40 @@ public class MemberService {
     }
 
     public Member findByMemberId(Long memberId) {
+        return getMember(memberId);
+    }
+
+    public Long Count() {
+        return memberRepository.count();
+    }
+
+    public List<AdminMemberDto> AdminFindMembers() {
+        List<Member> all = memberRepository.findAll();
+
+        return all.stream().map(AdminMemberDto::setForm).toList();
+    }
+
+    @Transactional
+    public void AdminMemberDelete(Long memberId) {
+        Member member = getMember(memberId);
+
+        memberRepository.delete(member);
+    }
+
+    public AdminMemberDto AdminFindMember(Long memberId) {
+        Member member = getMember(memberId);
+        return AdminMemberDto.setForm(member);
+    }
+
+    @Transactional
+    public void AdminChangeRole(Long memberId, String role) {
+        Member member = getMember(memberId);
+
+
+        member.changeRole(role);
+    }
+
+    private Member getMember(Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원 입니다."));
     }
