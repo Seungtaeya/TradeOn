ac8370f6a9b2c0358eec9bb5792934c411cffce2
seungtae <zkdlandgu@naver.com>
2025-07-30 23:50:34 +0900

    feat: IOException 추가

--- src/main/java/com/ecommerce/tradeon/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/tradeon/Exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.ecommerce.tradeon.Exceptions;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +15,11 @@ public class GlobalExceptionHandler {
 
         redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
         return "redirect:/login";
+    }
+
+    @ExceptionHandler(IOException.class)
+    public String IOException(IOException e , RedirectAttributes model) {
+        model.addFlashAttribute("errorMassage","상품 등록 실패했습니다. 다시 등록해주세요");
+        return "redirect:/product/new";
     }
 }
