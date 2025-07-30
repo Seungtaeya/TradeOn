6d01fc842a4e8206ea734ad7e5badce8c810cb2f
seungtae <zkdlandgu@naver.com>
2025-07-30 23:52:42 +0900

    feat:

--- src/main/java/com/ecommerce/tradeon/Controller/myPage/MyPageController.java
+++ src/main/java/com/ecommerce/tradeon/Controller/myPage/MyPageController.java
@@ -1,24 +1,33 @@
 package com.ecommerce.tradeon.Controller.myPage;
 
 import com.ecommerce.tradeon.Dto.Member.myPageMemberDto;
+import com.ecommerce.tradeon.Dto.Product.ProductDto;
 import com.ecommerce.tradeon.Dto.Session.SessionMember;
 import com.ecommerce.tradeon.Entity.Member.Member;
+import com.ecommerce.tradeon.Service.CategoryService;
 import com.ecommerce.tradeon.Service.MemberService;
+import com.ecommerce.tradeon.Service.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.net.http.HttpRequest;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MyPageController {
 
     private final MemberService memberService;
+    private final ProductService productService;
+    private final CategoryService categoryService;
 
     @GetMapping("/mypage")
     public String myPage(HttpSession session, RedirectAttributes redirectAttributes, Model model, HttpServletRequest request) {
@@ -30,9 +39,34 @@ public class MyPageController {
         }
 
         myPageMemberDto myPageMember = memberService.findMyPageMember(loginMember.getId());
+        List<ProductDto> productDtos = productService.MemberProduct(loginMember.getId());
 
         model.addAttribute("member",myPageMember);
-
+        model.addAttribute("myProducts", productDtos);
         return "Member/mypage";
+    }
+
+    @GetMapping("/mypage/product/{id}")
+    public String modifyProduct(@PathVariable(name = "id")Long productId, Model model) {
+        ProductDto productOne = productService.getProductOne(productId);
+
+        model.addAttribute("product", productOne);
+
+        return "Member/myProductDetail";
+    }
+
+    @GetMapping("/mypage/product/{id}/edit")
+    public String editProduct(@PathVariable(name = "id")Long productId, Model model) {
+        ProductDto productOne = productService.getProductOne(productId);
+
+        model.addAttribute("product", productOne);
+        return "Member/myPageEdit";
+    }
+
+    @PostMapping("/mypage/product/{id}/edit")
+    public String editProduct(@PathVariable(name = "id")Long productId,ProductDto productDto, List<MultipartFile> multipartFiles) {
+        productService.modifyProduct(productId,productDto,multipartFiles);
+
+        return "redirect:/product/detail/" + productId;
     }
 }
