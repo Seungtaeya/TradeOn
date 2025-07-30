13a327f53888dcbf05cefe391415cec02654c550
seungtae <zkdlandgu@naver.com>
2025-07-30 23:54:14 +0900

    feat:

--- src/main/java/com/ecommerce/tradeon/Repository/ProductRepository.java
+++ src/main/java/com/ecommerce/tradeon/Repository/ProductRepository.java
@@ -2,7 +2,12 @@ package com.ecommerce.tradeon.Repository;
 
 import com.ecommerce.tradeon.Entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("select p from Product p where p.seller_id.id = :memberId")
+    List<Product> findBySeller_id(Long memberId);
 }
