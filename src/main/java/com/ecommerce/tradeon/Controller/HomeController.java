d323ce505a5b8cbd6ced77404f3c57cd509a3070
seungtae <zkdlandgu@naver.com>
2025-07-30 23:51:30 +0900

    feat:

--- src/main/java/com/ecommerce/tradeon/Controller/HomeController.java
+++ src/main/java/com/ecommerce/tradeon/Controller/HomeController.java
@@ -1,15 +1,34 @@
 package com.ecommerce.tradeon.Controller;
 
+import com.ecommerce.tradeon.Dto.Category.CategoryDto;
+import com.ecommerce.tradeon.Dto.Product.ProductDto;
+import com.ecommerce.tradeon.Dto.Session.SessionMember;
+import com.ecommerce.tradeon.Service.CategoryService;
+import com.ecommerce.tradeon.Service.ProductService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+
+    private final CategoryService categoryService;
+    private final ProductService productService;
+
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<CategoryDto> categoryDtos = categoryService.listAll();
+        List<ProductDto> all = productService.findAll();
+
+        model.addAttribute("categories", categoryDtos);
+        model.addAttribute("latestProducts", all);
         return "home";
     }
 
