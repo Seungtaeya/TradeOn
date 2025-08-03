7c55004f2da11757524d831efbba233935a08003
seungtae <zkdlandgu@naver.com>
2025-08-04 03:14:50 +0900

    feat: 판매 주문 관리 기능

---
+++ src/main/java/com/ecommerce/tradeon/Repository/SaleOrderRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.ecommerce.tradeon.Repository;
+
+import com.ecommerce.tradeon.Dto.Order.SalesOrderDto;
+import com.ecommerce.tradeon.Entity.Member.QMember;
+import com.ecommerce.tradeon.Entity.Order.Order;
+import com.ecommerce.tradeon.Entity.Order.QOrder;
+import com.ecommerce.tradeon.Entity.Order.QOrderItem;
+import com.ecommerce.tradeon.Entity.product.QProduct;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class SaleOrderRepositoryImpl implements SaleOrderRepository{
+
+    private final JPAQueryFactory jpaQueryFactory;
+    QMember member = QMember.member;
+    QOrderItem oi = QOrderItem.orderItem;
+    QProduct product = QProduct.product;
+    QOrder order = QOrder.order;
+
+    @Override
+    public List<SalesOrderDto> findSalesOrderAll(Long memberId) {
+        List<Order> fetch = jpaQueryFactory
+                .selectFrom(order)
+                .join(order.orderItems, oi).fetchJoin()
+//                .join(oi.order, order).fetchJoin()
+                .where(product.seller_id.id.eq(memberId))
+                .fetch();
+
+        return fetch.stream()
+                .map(SalesOrderDto::setForm)
+                .toList();
+    }
+
+}
