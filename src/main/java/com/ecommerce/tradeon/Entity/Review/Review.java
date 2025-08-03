a3cccc2646c6d9b178e921336fc14fbcee70e0b1
seungtae <zkdlandgu@naver.com>
2025-08-04 03:14:17 +0900

    feat: 리뷰 관련 기능들

---
+++ src/main/java/com/ecommerce/tradeon/Entity/Review/Review.java
@@ -0,0 +1,50 @@
+package com.ecommerce.tradeon.Entity.Review;
+
+import com.ecommerce.tradeon.Entity.Member.Member;
+import com.ecommerce.tradeon.Entity.product.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    Member member;
+
+    private String content;
+    private Long rating;
+    private LocalDateTime createdAt;
+
+
+
+    public Review(String content, Long rating) {
+        this.content = content;
+        this.rating = rating;
+    }
+
+    public void assignProduct(Product product) {
+        this.product = product;
+    }
+
+    public void assignMember(Member member) {
+        this.member = member;
+    }
+
+    @PrePersist
+    public void createdAt() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
