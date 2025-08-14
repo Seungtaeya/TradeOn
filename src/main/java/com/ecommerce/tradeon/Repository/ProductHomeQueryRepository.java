1abff42056fc51bc3a6d5eb8c863f22de41371df
seungtae <zkdlandgu@naver.com>
2025-08-15 01:46:29 +0900

    feat: 홈 화면 상품 리스트 기능 추가

---
+++ src/main/java/com/ecommerce/tradeon/Repository/ProductHomeQueryRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tradeon.Repository;
+
+import com.ecommerce.tradeon.Dto.Product.ProductDto;
+
+import java.util.List;
+
+public interface ProductHomeQueryRepository {
+
+    List<ProductDto> findPopulars(int limit);
+    List<ProductDto> findMostView(int limit);
+    List<ProductDto> findUsed(int limit);
+    List<ProductDto> findBestSellers(int limit);
+}
