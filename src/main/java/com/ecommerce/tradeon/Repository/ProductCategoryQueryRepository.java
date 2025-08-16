b1a1bbd8e7bc1406ad59fed60c1517d90bbaabd8
seungtae <zkdlandgu@naver.com>
2025-08-17 01:01:24 +0900

    feat: 원하는 카테고리 누를 시 누른 카테고리로 등록된 상품 조회 기능 추기

---
+++ src/main/java/com/ecommerce/tradeon/Repository/ProductCategoryQueryRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.tradeon.Repository;
+
+import com.ecommerce.tradeon.Dto.Product.ProductDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductCategoryQueryRepository {
+
+    Page<ProductDto> findByCategoryId(Long categoryId, Pageable pageable);
+}
