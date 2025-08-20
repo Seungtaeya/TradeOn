908a883f1dda0e8cca9b753b4abcf427c840de76
seungtae <zkdlandgu@naver.com>
2025-08-20 23:38:15 +0900

    feat:

---
+++ src/main/java/com/ecommerce/tradeon/Exceptions/CustomBannerException.java
@@ -0,0 +1,11 @@
+package com.ecommerce.tradeon.Exceptions;
+
+public class CustomBannerException extends RuntimeException{
+
+    public CustomBannerException() {
+    }
+
+    public CustomBannerException(String message) {
+        super(message);
+    }
+}
