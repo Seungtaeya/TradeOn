356e4f6ec1f3a69b7cf3842d5089c4c809adb583
seungtae <zkdlandgu@naver.com>
2025-08-20 23:37:13 +0900

    feat: 배너 기능 추가

--- src/main/java/com/ecommerce/tradeon/Service/BannerService.java
+++ src/main/java/com/ecommerce/tradeon/Service/BannerService.java
@@ -2,6 +2,7 @@ package com.ecommerce.tradeon.Service;
 
 import com.ecommerce.tradeon.Dto.Banner.BannerDto;
 import com.ecommerce.tradeon.Entity.Banner.Banner;
+import com.ecommerce.tradeon.Exceptions.CustomBannerException;
 import com.ecommerce.tradeon.Repository.BannerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class BannerService {
     private final BannerRepository bannerRepository;
 
     @Transactional
-    public void saveBanner(BannerDto bannerDto) throws IOException {
+    public void saveBanner(BannerDto bannerDto) {
 
         String uploadDir = "/Users/seungtae/IdeaProjects/TradeOn/banner";
 
@@ -28,16 +29,36 @@ public class BannerService {
 
         File destfile = new File(uploadDir, fileName);
 
-        bannerDto.getImage().transferTo(destfile);
+        try {
+            bannerDto.getImage().transferTo(destfile);
+
+        } catch (IOException e) {
+            throw new CustomBannerException(e.getMessage());
+        }
 
         String savePath = "/banner/" + fileName;
 
-        Banner banner = new Banner(savePath);
+        Banner banner = new Banner(savePath, bannerDto.getImageName());
 
-        bannerRepository.save(banner);
+
+        Banner save = bannerRepository.save(banner);
     }
 
     public List<BannerDto> getBanners() {
         return bannerRepository.findAll().stream().map(BannerDto::setForm).toList();
+    }
+
+    public BannerDto getBanner(Long bannerId) {
+        Banner banner = bannerRepository.findById(bannerId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이벤트 입니다."));
+        return BannerDto.setForm(banner);
+    }
+
+    @Transactional
+    public void deleteBanner(Long bannerId) {
+        Banner banner = bannerRepository.findById(bannerId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이벤트 입니다."));
+
+        bannerRepository.delete(banner);
     }
 }
